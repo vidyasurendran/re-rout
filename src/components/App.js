262360264129f7c20838fc7d3b297a5ebcee4198
Vidya A. Surendran <Vidya.Surendran@marlabs.com>
2018-07-11 15:24:57 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <ul className="routes">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Store</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Subscibe</a></li>
+            <li><a href="">Customer-Support</a></li>
+            <li><a href="">My Account</a></li>
+            </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
